517a29d62ded93848b4c021d77875c321c07d3eb
lee <shirlin@fofo.tw>
2020-09-07 19:51:29 +0800

    init

---
+++ assets/js/allfunc.js
@@ -0,0 +1,18 @@
+var allFunc = {
+    addCommas : function (val) {
+        while (/(\d+)(\d{3})/.test(val.toString())) { val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2'); } return val;
+    },
+    getUrlParam : function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null)
+            return unescape(r[2]);
+        return null;
+    },
+    randomFunc: function (min,max){
+        var maxNum = max;  
+        var minNum = min;  
+        var n = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;  
+        return n;
+    }
+}
